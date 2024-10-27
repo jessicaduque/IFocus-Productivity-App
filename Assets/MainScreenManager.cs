36efe92d67ffd6984acf205381853d7a11c27455
jessica.duque@sweetmedia.com.br <jduquedeveloper@gmail.com>
2024-10-27 13:25:08 -0300

    Mais do fluxo principal

---
+++ Assets/MainScreenManager.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScreenManager : MonoBehaviour
+{
+    [SerializeField] private Button b_alarm, b_computer;
+    private UIPanelsManager _uiPanelsManager => UIPanelsManager.I;
+
+    private void Awake()
+    {
+        SetupButtons();
+    }
+
+    private void SetupButtons()
+    {
+        b_alarm.onClick.AddListener(delegate { _uiPanelsManager.ControlAlarmPanel(true); });
+        b_computer.onClick.AddListener(delegate { _uiPanelsManager.ControlComputerScreenPanel(true); });
+    }
+    
+}
