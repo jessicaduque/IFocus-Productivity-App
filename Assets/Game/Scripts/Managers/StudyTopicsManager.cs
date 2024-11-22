3b5836c735b1ede659a01599e836dd486ca88669
jessica.duque@sweetmedia.com.br <jduquedeveloper@gmail.com>
2024-11-22 13:27:07 -0300

    Melhoria Scroll Rect / UI timer main na scene Gigi

--- Assets/Game/Scripts/Managers/StudyTopicsManager.cs
+++ Assets/Game/Scripts/Managers/StudyTopicsManager.cs
@@ -1,19 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Utils.Singleton;
 
 public class StudyTopicsManager : Singleton<StudyTopicsManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Transform _content;
+    [SerializeField] Button b_create;
+    [SerializeField] GameObject _todoListItemPrefab;
+    [SerializeField] TMP_InputField _addInputField;
+    
+    int _amountListObjects = 0;
 }
--- Assets/Game/Scripts/UI/InfiniteVerticalTimeScroll/TimeScrollPart.cs
+++ Assets/Game/Scripts/UI/InfiniteVerticalTimeScroll/TimeScrollPart.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TimeScrollPart : MonoBehaviour
+public class TimeScrollPart : MonoBehaviour, IEndDragHandler
 {
     [SerializeField] UnityEngine.UI.LoopVerticalScrollRect scrollRect;
+    private Transform _content;
+    private int _currentValue;
 
+    void Awake()
+    {
+        _content = scrollRect.content;
+    }
+    
     private IEnumerator Start()
     {
         yield return new WaitForSeconds(0.01f);
         scrollRect.ScrollToCellWithinTime(0, 0.01f);
     }
-    
-    
 
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log(scrollRect.GetLastItem(out float test));
+        scrollRect.ScrollToCellWithinTime(scrollRect.GetLastItem(out float offset), 0.1f);
+        _currentValue = scrollRect.GetLastItem(out offset);
+    }
 }
