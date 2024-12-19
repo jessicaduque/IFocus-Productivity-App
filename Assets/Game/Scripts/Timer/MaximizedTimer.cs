600512be1297d7ef38d4828eee5b2e237852f1c7
Jessica Nogueira Duque <69450467+jessicaduque@users.noreply.github.com>
2024-12-19 10:02:19 -0300

    Update MaximizedTimer.cs with comments

--- Assets/Game/Scripts/Timer/MaximizedTimer.cs
+++ Assets/Game/Scripts/Timer/MaximizedTimer.cs
@@ -3,77 +3,77 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-using Utils.Singleton;
+using Utils.Singleton; // Script de Singleton criado para herdar e ser chamado com facilidade
 public class MaximizedTimer : Singleton<MaximizedTimer>
 {
-    [Header("UI Components")]
+    [Header("UI Components")] // Componentes de UI para controlar o timer maximizado
     [SerializeField] private GameObject setTimerText;
     [SerializeField] private Image uiFill;
-    [Header("Buttons")]
+    [Header("Buttons")] // Botões do alarme para manipulá-lo
     [SerializeField] private Button playButton;
     [SerializeField] private Button pauseButton;
     [SerializeField] private Button quitButton;
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button generalLeaveButton;
-    [Header("Input Fields")] 
+    [Header("Input Fields")] // Input fields do timer para input do usuário e definição do timer
     [SerializeField] private TMP_InputField hoursInputField;
     [SerializeField] private TMP_InputField minutesInputField;
     [SerializeField] private TMP_InputField secondsInputField;
-    [Header("Texts")]
+    [Header("Texts")] // Textos que estão no lugar do input field para demonstrar melhor ao usuário
     [SerializeField] private TextMeshProUGUI hoursText;
     [SerializeField] private TextMeshProUGUI minutesText;
     [SerializeField] private TextMeshProUGUI secondsText;
 
-    private float totalSeconds;
-    
-    public UnityAction playTimerAction, resumeTimerAction, pauseTimerAction, quitTimerAction;
-    private UIPanelsManager _uiPanelsManager => UIPanelsManager.I;
-    private TimerManager _timerManager => TimerManager.I;
-    
+    private float totalSeconds; // Total de segundos do timer quando é definido 
 
+    private UIPanelsManager _uiPanelsManager => UIPanelsManager.I; // Pegar o singleton do UIPanelsManager para controlar o painel de timer maximizado
+    private TimerManager _timerManager => TimerManager.I; // Pegar o singleton do TimerManager que controla dados sobre o timer, principalmente considerando quando o painel de TimerMaximizado estará inativo
+    
+    // Função default do Unity que roda uma única vez quando um objeto ativo está sendo carregado 
     private new void Awake()
     {
-        SetupButtons();
+        SetupButtons(); // Função para definir o que cada botão faz no início (mantendo assim maior controle dos componentes)
     }
-
+    
+    // Função default do Unity que roda toda vez que o objeto ligado ao script é ativado
     private void OnEnable()
     {
-        _timerManager.endTimerAction += InitialUI;
+        _timerManager.endTimerAction += InitialUI; // Inscreve a função de UI na ação de fim de timer para atualizar a UI ao estado primário dele
         
-        if(_timerManager.GetTimerState() == TIMER_STATE.TIMER_OFF)
+        if(_timerManager.GetTimerState() == TIMER_STATE.TIMER_OFF) // Se o timer estiver no estado desligado, atualiza a UI à seu estado primário
             InitialUI();
     }
-
+    // Função default do Unity que roda toda vez que o objeto ligado ao script é desativado
     private void OnDisable()
     {
-        _timerManager.endTimerAction -= InitialUI;
+        _timerManager.endTimerAction -= InitialUI; // Desinscreve a funçãon de UI inicial à ação de fim de timer para ele não ser chamado  se o painel está desativado
     }
-
+    // Função para setar o total de segundos qunando um timer novo é definido
     private void SetTotalSeconds()
     {
         totalSeconds = 0;
-        _timerManager.SetTotalSeconds(totalSeconds);
-        
+    
+        // Cálculo para mostrar as horas faltando ao usuário
         if(hoursInputField.text != "" && hoursInputField.text != " ")
             totalSeconds += int.Parse(hoursInputField.text) * 3600;
-        
+        // Cálculo para mostrar os minutos faltando ao usuário
         if(minutesInputField.text != "" && minutesInputField.text != " ")
             totalSeconds += int.Parse(minutesInputField.text) * 60;
-        
+        // Cálculo para mostrar os segundos faltando ao usuário
         if(secondsInputField.text != "" && secondsInputField.text != " ")
             totalSeconds += int.Parse(secondsInputField.text);
         
-        totalSeconds += 1;
+        totalSeconds += 1; // Gambiarra para que quando o timer iniciar, o timer não começar visualmente com um segundo a menos e o usuário estranhar
         
-        _timerManager.SetTotalSeconds(totalSeconds);
+        _timerManager.SetTotalSeconds(totalSeconds); // Seta o total de segundos no TimerManager para controlar as informações de segundos do timer
     }
-
+    // Função para setup inicial dos botões com seus funcionamentos
     private void SetupButtons()
     {
-        playButton.onClick.AddListener(Play);
-        pauseButton.onClick.AddListener(Pause);
-        resumeButton.onClick.AddListener(Resume);
-        quitButton.onClick.AddListener(Quit);
+        playButton.onClick.AddListener(Play); // Adiciona o método de play quando o timer é ligado
+        pauseButton.onClick.AddListener(Pause); // Adiciona o método de pausa quando o timer é pausado
+        resumeButton.onClick.AddListener(Resume); // Adiciona o método de resumir o timer quando é resumido
+        quitButton.onClick.AddListener(Quit); 
         generalLeaveButton.onClick.AddListener(delegate { _uiPanelsManager.ControlAlarmPanel(false); ControlStateButtons(true); });
     }
     
