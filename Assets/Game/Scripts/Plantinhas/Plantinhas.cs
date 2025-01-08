126b4cde4271d6687654ba6d57e0979b7df23b08
gscalfoni <104603818+gscalfoni@users.noreply.github.com>
2025-01-08 19:49:26 -0300

    Trocar de imagem onclick

--- Assets/Game/Scripts/Plantinhas/Plantinhas.cs
+++ Assets/Game/Scripts/Plantinhas/Plantinhas.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Plantinhas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Plant
     {
-        
+        public GameObject plantObject; 
+        public Sprite healthySprite;  
+        public Sprite wiltedSprite;
+        [HideInInspector] public bool isWilted = false;
+    }
+    public Plant[] plants;
+    private void Start()
+    {
+        foreach (var plant in plants)
+        {
+            SetPlantHealthy(plant);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                foreach (var plant in plants)
+                {
+                    if (hit.collider.gameObject == plant.plantObject)
+                    {
+                        TogglePlantState(plant);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void SetPlantHealthy(Plant plant)
+    {
+        plant.isWilted = false;
+        plant.plantObject.GetComponent<SpriteRenderer>().sprite = plant.healthySprite;
+    }
+
+    private void SetPlantWilted(Plant plant)
+    {
+        plant.isWilted = true;
+        plant.plantObject.GetComponent<SpriteRenderer>().sprite = plant.wiltedSprite;
+    }
+
+    private void TogglePlantState(Plant plant)
+    {
+        if (plant.isWilted)
+        {
+            SetPlantHealthy(plant);
+        }
+        else
+        {
+            SetPlantWilted(plant);
+        }
     }
 }
