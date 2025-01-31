d4740d5cd62e2aa072ed815a780e2a5fd16ed5aa
jessica.duque@sweetmedia.com.br <jduquedeveloper@gmail.com>
2025-01-31 20:36:30 -0300

    Preparo para controle tempos do dia

---
+++ Assets/Game/Scripts/Managers/UITimeOfDayFilterController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITimeOfDayFilterController : MonoBehaviour
+{
+    // 3 imagens
+    // Manhã: 5am-11am, tarde -12pm-6pm, noite 7pm-4am
+    // No update: checar sempre o horário atual, e quanto bater um horário de certo tempo, mudar a imagem que cobre a tela.
+    // Três imagens desativadas, elas devem ser ativadas de acordo.
+    // Setar o correto quando o app começa (no Awake)
+    
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
